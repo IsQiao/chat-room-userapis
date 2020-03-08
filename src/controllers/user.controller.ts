f771c986da9eb8d9c9bca3d876b5aa6f841b6ab0
Qiao <qxwxintu@gmail.com>
2020-03-09 02:49:32 +0800

    getUserInfoByToken api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get, Route, Query, OperationId, Tags } from 'tsoa';
 @Tags('用户API')
 export class UserController extends Controller {
     /**
-     * 
+     * 根据多个用户Id查询多个用户信息
      * @param ids 多个用户Id(数组)
      */
     @Get('usersInfo')
@@ -17,6 +17,20 @@ export class UserController extends Controller {
                 avatar: "avatar01.jpg"
             }
         });
+    }
+
+    /**
+     * 根据用户到token查询用户信息
+     * @param token 跳转到聊天服务时带过来到token
+     */
+    @Get('{token}')
+    public async getUserInfoByToken(token: string): Promise<UserInfo> {
+        return {
+            id: 'userid需要保持唯一切不会改变',
+            nickName: `nickName_${token}`,
+            activeValue: 66,
+            avatar: "avatar01.jpg"
+        }
     }
 }
 
